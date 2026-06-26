29613d224a693062e2f2558fe286098971487226
Mauricio Lima <mauricio@collateralxp.com>
2026-06-25 22:26:09 -0300

    More and final improvements
    - solve magic numbers
    - Item class now doesn't allow changes outside the class
    - improve with aux methods the Backstage class

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,5 +1,8 @@
 class GildedRose
   class BaseItem
+    SELL_DATE_PASSED_INCREMENT = 2
+    DEFAULT_INCREMENT = 1
+
     attr_reader :item
 
     def initialize(item)
@@ -13,10 +16,10 @@ class GildedRose
     def update_quality
       item.decrease_sell_in
 
-      if item.sell_in.positive?
+      if item.before_sell_date?
         item.decrease_quality
       else
-        item.decrease_quality(2)
+        item.decrease_quality(SELL_DATE_PASSED_INCREMENT)
       end
     end
   end
@@ -25,10 +28,10 @@ class GildedRose
     def update_quality
       item.decrease_sell_in
 
-      if item.sell_in.positive?
+      if item.before_sell_date?
         item.increase_quality
       else
-        item.increase_quality(2)
+        item.increase_quality(SELL_DATE_PASSED_INCREMENT)
       end
     end
   end
@@ -37,29 +40,43 @@ class GildedRose
   end
 
   class Backstage < BaseItem
+    THREE_X_RANGE = (-Float::INFINITY)..5
+    TWO_X_RANGE = 6..10
+    THREE_X_INCREMENT = 3
+    TWO_X_INCREMENT = 2
+
     def update_quality
       item.decrease_sell_in
 
-      if item.sell_in.negative?
+      if item.sell_date_passed?
         item.decrease_quality(item.quality)
-      elsif item.sell_in <= 5
-        item.increase_quality(3)
-      elsif item.sell_in <=10
-        item.increase_quality(2)
       else
-        item.increase_quality
+        item.increase_quality(backstage_increment_for(item.sell_in))
+      end
+    end
+
+    private
+
+    def backstage_increment_for(sell_in)
+      case sell_in
+      when THREE_X_RANGE then THREE_X_INCREMENT
+      when TWO_X_RANGE then TWO_X_INCREMENT
+      else DEFAULT_INCREMENT
       end
     end
   end
 
   class Conjured < BaseItem
+    SELL_DATE_PASSED_INCREMENT = 4
+    DEFAULT_INCREMENT = 2
+
     def update_quality
       item.decrease_sell_in
 
-      if item.sell_in.negative?
-        item.decrease_quality(4)
+      if item.sell_date_passed?
+        item.decrease_quality(SELL_DATE_PASSED_INCREMENT)
       else
-        item.decrease_quality(2)
+        item.decrease_quality(DEFAULT_INCREMENT)
       end
     end
   end
@@ -73,20 +90,19 @@ class GildedRose
     @items = items
   end
 
-  def update_quality
-    @items.each { |item| update_item_quality(item) }
-  end
+  def update_quality = @items.each { |item| update_item_quality(item) }
 
   private
 
   def update_item_quality(item)
     strategy = STRATEGIES.fetch(item.name, Normal)
+
     strategy.new(item).update_quality
   end
 end
 
 class Item
-  attr_accessor :name, :sell_in, :quality
+  attr_reader :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
     @name = name
@@ -95,22 +111,26 @@ class Item
   end
 
   def decrease_quality(qty = 1)
-    @quality -= qty
+    self.quality -= qty
 
-    @quality = 0 if @quality < 0
+    self.quality = 0 if quality < 0
   end
 
   def increase_quality(qty = 1)
-    @quality += qty
+    self.quality += qty
 
-    @quality = 50 if @quality > 50
+    self.quality = 50 if quality > 50
   end
 
   def decrease_sell_in
-    @sell_in -= 1
+    self.sell_in -= 1
   end
 
-  def to_s
-    "#{@name}, #{@sell_in}, #{@quality}"
-  end
+  def sell_date_passed? = sell_in.negative?
+  def before_sell_date? = sell_in.positive? || sell_in.zero?
+  def to_s = "#{name}, #{sell_in}, #{quality}"
+
+  private
+
+  attr_writer :sell_in, :quality
 end
